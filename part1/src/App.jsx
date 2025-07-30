ae6aaa0647ce5ebb3fae39a404ca6546d0f841bb
Swikar Ramdam <swikarramdam@gmail.com>
2025-07-30 08:25:01 +0545

    changed the name

---
+++ part1/src/App.jsx
@@ -0,0 +1,12 @@
+import SayHello from "./SayHello";
+
+const App = () => {
+  return (
+    <>
+    <h1 className="swikarClass">Hello Swikar</h1>,
+    <SayHello name = "Swikar" />
+    </>
+  );
+};
+export default App;
+
---
+++ part1/src/SayHello.js
@@ -0,0 +1,5 @@
+const SayHello = (props) => {
+  return <p>Hello {props.name}</p>;
+};
+
+export default SayHello;
